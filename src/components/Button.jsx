eb16c0d379d06247d5084903b1076541583cac73
Caique <caiquewin@gmail.com>
2022-02-07 21:09:38 -0300

    Adicionando backGroudColor nos buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import './Button.css'
 
 export default props =>{ 
+    let color =props.color?props.color:"withe" 
+    
     return(
-       <button className="button">
+       <button className="button" style={{
+           backgroundColor:`${color}`
+           }}>
         {props.label}
        </button>
     )
